d75b45f78e0be701172a909c900c16b8727a4dfd
omnihonk <heimburger.robert@gmail.com>
2026-05-16 14:50:56 +0100

    Init

---
+++ dependencies.py
@@ -0,0 +1,10 @@
+from typing import Generator
+
+from sqlmodel import Session
+
+from database.db import engine
+
+
+def get_session() -> Generator:
+    with Session(engine) as session:
+        yield session
